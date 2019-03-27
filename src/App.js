5fb65769d89465bf465e76934c0c55e09623040f
James <renatovisuals@gmail.com>
2019-03-27 13:05:57 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import MapContainer from './components/MapContainer/MapContainer';
+import markerData from './db.js';
+
+class App extends Component {
+    state = {
+        markerData:markerData()
+    }
+  render() {
+    return (
+      <div className="App">
+          <MapContainer markerData = {this.state.markerData} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/db.js
@@ -0,0 +1,10 @@
+export default function(){
+  return[
+      {
+          id: 1,
+          name:"Home",
+          address:"3100 Hamilton Ave",
+          position:{lat:32.7547,lng:-97.3614}
+      }
+  ]
+}
